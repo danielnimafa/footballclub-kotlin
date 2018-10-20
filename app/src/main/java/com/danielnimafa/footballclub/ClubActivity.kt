8ba7130057e1894d0e770993ba81da46d70b5bda
danielnimafa <danielnimafa@gmail.com>
2018-10-20 07:31:34 +0700

    create projects

---
+++ app/src/main/java/com/danielnimafa/footballclub/ClubActivity.kt
@@ -0,0 +1,12 @@
+package com.danielnimafa.footballclub
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ClubActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_club)
+    }
+}
